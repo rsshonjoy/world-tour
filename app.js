97d6e243733f659c3484f3dccf5d3f9d9a35bf4a
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-06 10:16:40 +0600

    use shortcut

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ const displayCountries = countries =>{
         const country = countries[i];
         const countryDiv = document.createElement('div');
 
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
 
-        const p = document.createElement('p');
-        p.innerText = country.capital;
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
 
-        countryDiv.appendChild(h3)
-        countryDiv.appendChild(p)
+        // countryDiv.appendChild(h3)
+        // countryDiv.appendChild(p)
 
+        const countryInfo = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+        `
+        countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     }
 }
