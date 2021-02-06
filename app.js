be21c5546d04b7335efcb2c2a7c71764164fbbc3
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-06 10:54:51 +0600

    call dynamic API

--- app.js
+++ app.js
@@ -13,21 +13,15 @@ const displayCountries = countries =>{
         const countryInfo = `
             <h3 class="country-name">${country.name}</h3>
             <p>${country.capital}</p>
+            <button onclick="displayCountryDetail('${country.name}')">Details</button>
         `
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     });
-    // for (let i = 0; i < countries.length; i++) {
-    //     const country = countries[i];
-    //     const countryDiv = document.createElement('div');
+}
 
-    //     countryDiv.className = 'country';
-
-    //     const countryInfo = `
-    //         <h3 class="country-name">${country.name}</h3>
-    //         <p>${country.capital}</p>
-    //     `
-    //     countryDiv.innerHTML = countryInfo;
-    //     countriesDiv.appendChild(countryDiv);
-    // }
+const displayCountryDetail = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    console.log(url);
+    
 }
